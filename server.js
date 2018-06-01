8b33e6dc2af759a18996fec222c6f66094ee6c27
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-01 14:11:12 -0700

    finished section 5 lesson 45 express middleware

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+//setting the view engine to hbs for express
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '/n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+// maintenance middleware - comment out when not in use b/c it stops everything underneath it from rendering
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+// setting up static files (html, css, js, images); all static files can be served from this folder
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
 });
 
+// hbs helper that returns the targeted title (ie welcomeMessage) as all uppercase
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
+// routing for home page with request and response parameter
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>'); example
     res.render('home.hbs', {
@@ -23,18 +47,21 @@ app.get('/', (req, res) => {
     });
 });
 
+// routing for about page
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
     });
 });
 
+// routing for error page
 app.get('/bad', (req,res) => {
     res.send({
         errorMessage: 'Unable to handle request'
     });
 });
 
+// listing on port 3000
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
