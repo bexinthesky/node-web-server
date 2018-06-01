30fb9ca28a26e6eda47a1b2a9ef13da9dbc9b94b
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-01 12:35:28 -0700

    finished section 5 lecture 44 - advanced templating

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
-const hbs = ('hbs');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
+    // res.send('<h1>Hello Express!</h1>'); example
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to my website',
-        currentYear: new Date().getFullYear(),
-    })
+    });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     });
 });
 
